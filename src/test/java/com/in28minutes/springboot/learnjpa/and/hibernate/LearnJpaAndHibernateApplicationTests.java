588128d3ba1f2ece6ba711fd1fe2bd5c9fa967e7
vineeshagundeboina <gundeboinavineesha123@gmail.com>
2024-12-15 17:20:23 +0530

    learnjpaandhibernate commit

---
+++ src/test/java/com/in28minutes/springboot/learnjpa/and/hibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learnjpa.and.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
